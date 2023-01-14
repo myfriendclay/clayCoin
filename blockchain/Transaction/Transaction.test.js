c6911c70c6f9f2aa5d4e7d75c9c994a0b7e871d9
Clay Blackiston <clay@Clays-MacBook-Pro.local>
2023-01-13 17:46:46 -0800

    Refactoring Transaction class all tests passing

--- blockchain/Transaction/Transaction.test.js
+++ blockchain/Transaction/Transaction.test.js
@@ -7,9 +7,6 @@ const key = ec.genKeyPair();
 const publicKey = key.getPublic('hex');
 const privateKey = key.getPrivate('hex');
 
-console.log(publicKey)
-console.log(privateKey)
-
 const key2 = ec.genKeyPair();
 const publicKey2 = key.getPublic('hex');
 const privateKey2 = key.getPrivate('hex');
@@ -31,56 +28,52 @@ describe('Constructor function (creation)', () => {
   });
 });
 
+describe('calculateHash', () => {
+  it('Returns a valid hash of transaction', () => {
+    expect(newTransaction.calculateHash()).toBe('2d42a23585224da9e3a6c5001c64e989d83690f2c75e6b0e03ed7ec5153d07e2')
+  })
 
-  describe('calculateHash', () => {
-    it('Returns a valid hash of transaction', () => {
-      expect(newTransaction.calculateHash()).toBe('2d42a23585224da9e3a6c5001c64e989d83690f2c75e6b0e03ed7ec5153d07e2')
-    })
+  it('Changes hash if you tamper with any part of transaction', () => {
+    const firstHash = newTransaction.calculateHash()
+    newTransaction.amount = 99999
+    expect(newTransaction.calculateHash()).not.toBe(firstHash)
 
-    it('Changes hash if you tamper with any part of transaction', () => {
-      const firstHash = newTransaction.calculateHash()
-      newTransaction.amount = 99999
-      expect(newTransaction.calculateHash()).not.toBe(firstHash)
+    const secondHash = newTransaction.calculateHash()
+    newTransaction.toAddress = "tamperedToAddress"
+    expect(newTransaction.calculateHash()).not.toBe(secondHash)
 
-      const secondHash = newTransaction.calculateHash()
-      newTransaction.toAddress = "tamperedToAddress"
-      expect(newTransaction.calculateHash()).not.toBe(secondHash)
+    const thirdHash = newTransaction.calculateHash()
+    newTransaction.fromAddress = "tamperedFromAddress"
+    expect(newTransaction.calculateHash()).not.toBe(thirdHash)
+  })
 
-      const thirdHash = newTransaction.calculateHash()
-      newTransaction.fromAddress = "tamperedFromAddress"
-      expect(newTransaction.calculateHash()).not.toBe(thirdHash)
-
-    })
   });
 
   describe('signTransaction', () => {
     test('Throws error if not fromAddress signing', () => {
-      const newTransaction = new Transaction("bogus_from_address", "to_address", 45)
       expect(() => newTransaction.signTransaction(key)).toThrow(Error)
     });
 
     test('Updates Transaction signature when signingKey matches fromAddress', () => {
-      const newTransaction = new Transaction(publicKey, "bogus_to_address", 45)
-      newTransaction.signTransaction(key)
+      newTransaction.fromAddress = publicKey
+      newTransaction.signTransaction(privateKey)
       expect(newTransaction).toHaveProperty('signature')
     });
 
   });
 
   describe('isValid', () => {
-    
     test('If fromAddress is "Coinbase Tx" then returns true (mining reward', () => {
-      const newTransaction = new Transaction("Coinbase Tx", "to_address", 45)
+      newTransaction.fromAddress = "Coinbase Tx"
       expect(newTransaction.isValid()).toBe(true)
     });
 
     test('Throws error if missing signature', () => {
-      const newTransaction = new Transaction("from_address", "to_address", 45)
       expect(() => newTransaction.isValid()).toThrow(Error)
     });
     
     test('Returns false if signature doesnt match fromAddress public key', () => {
-      const newTransaction = new Transaction(publicKey, "to_address", 45)
+      newTransaction.fromAddress = publicKey
       const different_key = ec.genKeyPair();
       const transactionHash = newTransaction.calculateHash()
       const different_signature = different_key.sign(transactionHash, 'base64')
@@ -89,8 +82,8 @@ describe('Constructor function (creation)', () => {
     });
 
     test('Returns true if signature matches fromAddress public key', () => {
-      const newTransaction = new Transaction(publicKey, "to_address", 45)
-      newTransaction.signTransaction(key)
+      newTransaction.fromAddress = publicKey
+      newTransaction.signTransaction(privateKey)
       expect(newTransaction.isValid()).toBe(true)
     });
 
