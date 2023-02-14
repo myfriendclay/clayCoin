5e693697893377691d9b80a0de48be5e959bea24
Clay Blackiston <clay@Clays-MacBook-Pro.local>
2023-02-13 22:19:15 -0800

    Refactor transaction test class

--- blockchain/Transaction/Transaction.test.js
+++ blockchain/Transaction/Transaction.test.js
@@ -6,35 +6,52 @@ import { COINBASE_TX } from "../../config";
 import {CoinbaseTransaction} from './Transaction'
 
 let newTransaction
-let publicKey
-let privateKey
+const publicKey = "0466106d7a83b41134c3e973fe13c0aa682ec8ee94c32cf6d66429e429554dc4ed9d7c64d0c5685229f503307a30f65c954e781d0f3cf4f77d07e0a9d4c37e1d02"
+const privateKey = "0b9e25155f05ba792d8ed7d670201e60959184af77ab433ea814b67422276149"
+const transactionFields = {
+  fromAddress: publicKey,
+  toAddress: "bogus_to_address",
+  amount: 45,
+  memo: "pizza and beer",
+  uuid: "123456789"
+}
+const txHash = "b44414faa605d21b826315db81c64897ddd0c00f1abc06b37ea90c35d97ae448"
+const txSignature = "30450221009053cc684f78c5173d389874fa556fde4f3314685650e58b89c1ac5ef920fc4c022042aae57e122bf5b13e75f45db4c3b1c59e7210b72197fa9b40ac3ca2955d395a"
+
+const {fromAddress, toAddress, amount, memo, uuid } = transactionFields
 
 beforeEach(() => {
-  publicKey = "0466106d7a83b41134c3e973fe13c0aa682ec8ee94c32cf6d66429e429554dc4ed9d7c64d0c5685229f503307a30f65c954e781d0f3cf4f77d07e0a9d4c37e1d02"
-  privateKey = "0b9e25155f05ba792d8ed7d670201e60959184af77ab433ea814b67422276149"
-  newTransaction = new Transaction("bogus_from_address", "bogus_to_address", 45, "pizza and beer")
-  newTransaction.uuid = "123456789"
+  newTransaction = new Transaction(fromAddress, toAddress, amount, memo)
+  newTransaction.uuid = uuid
+  newTransaction.timestamp = 1
 });
 
 describe('Constructor function (creation)', () => {
-  it('Create Transaction with all 6 properties', () => {
+
+  it('Creates instance of Transaction class', () => {
+    expect(newTransaction).toBeInstanceOf(Transaction);
+  });
+
+  it('Creates Transaction with all properties', () => {
     expect(newTransaction).toHaveProperty('fromAddress');
     expect(newTransaction).toHaveProperty('toAddress');
     expect(newTransaction).toHaveProperty('amount');
     expect(newTransaction).toHaveProperty('memo');
-    expect(newTransaction).toHaveProperty('timestamp');
+    expect(newTransaction).toHaveProperty('fee');
     expect(newTransaction).toHaveProperty('uuid');
+    expect(newTransaction).toHaveProperty('timestamp');
   });
 
   it('Accurately sets all of the fields', () => {
-    expect(newTransaction.fromAddress).toBe('bogus_from_address');
-    expect(newTransaction.toAddress).toBe('bogus_to_address');
-    expect(newTransaction.amount).toBe(45);
-    expect(newTransaction.memo).toBe("pizza and beer");
-    expect(newTransaction.uuid).toBe("123456789");
+    expect(newTransaction.fromAddress).toBe(fromAddress);
+    expect(newTransaction.toAddress).toBe(toAddress);
+    expect(newTransaction.amount).toBe(amount);
+    expect(newTransaction.memo).toBe(memo);
+    expect(newTransaction.uuid).toBe(uuid);
   });
 
   it('Automatically creates a timestamp', () => {
+    newTransaction = new Transaction(fromAddress, toAddress, amount, memo)
     const minTime = Date.now() - 10
     const maxTime = Date.now() + 10
     expect(newTransaction.timestamp).toBeGreaterThanOrEqual(minTime);
@@ -43,42 +60,38 @@ describe('Constructor function (creation)', () => {
 });
 
 describe('calculateHash', () => {
+  let initialHash
+  
   beforeEach(() => {
-    newTransaction.timestamp = 1
+    initialHash = newTransaction.calculateHash()
   });
 
   it('Changes hash if fromAddress changes', () => {
-    const initialHash = newTransaction.calculateHash()
     newTransaction.fromAddress = "tamperedFromAddress"
     expect(newTransaction.calculateHash()).not.toBe(initialHash)
   })
 
   it('Changes hash if toAddress changes', () => {
-    const initialHash = newTransaction.calculateHash()
     newTransaction.toAddress = "tamperedToAddress"
     expect(newTransaction.calculateHash()).not.toBe(initialHash)
   })
 
   it('Changes hash if amount changes', () => {
-    const initialHash = newTransaction.calculateHash()
     newTransaction.amount = 999999
     expect(newTransaction.calculateHash()).not.toBe(initialHash)
   })
 
   it('Changes hash if memo changes', () => {
-    const initialHash = newTransaction.calculateHash()
     newTransaction.memo = "tamperedMemo"
     expect(newTransaction.calculateHash()).not.toBe(initialHash)
   })
 
   it('Changes hash if timestamp changes', () => {
-    const initialHash = newTransaction.calculateHash()
     newTransaction.timestamp = "tamperedtimestamp"
     expect(newTransaction.calculateHash()).not.toBe(initialHash)
   })
 
   it('Changes hash if uuid changes', () => {
-    const initialHash = newTransaction.calculateHash()
     newTransaction.uuid = "tamperedUUID"
     expect(newTransaction.calculateHash()).not.toBe(initialHash)
   })
@@ -90,34 +103,37 @@ describe('calculateHash', () => {
     expect(newTransaction.calculateHash()).toBe(mockedReturnValue)
     expect(getSHA256HashModule.default).toHaveBeenCalledWith(fromAddress, toAddress, amount, memo, fee, uuid, timestamp)
   })
-
-  });
+});
 
 describe('signTransaction', () => {
-  beforeEach(() => {
-    newTransaction.timestamp = 1
-  });
 
   it('Throws error if not fromAddress signing', () => {
-    expect(() => newTransaction.signTransaction(key)).toThrow(Error)
+    expect(() => newTransaction.signTransaction("randomPrivateKey")).toThrow(Error)
   });
 
   it('Updates Transaction signature when signingKey matches fromAddress', () => {
-    newTransaction.fromAddress = publicKey
+    jest.spyOn(newTransaction, "calculateHash").mockReturnValue(txHash)
     newTransaction.signTransaction(privateKey)
     expect(newTransaction).toHaveProperty('signature')
   });
 
-  test.todo('Updates Transaction with valid signature')
+  it('Updates Transaction with valid signature', () => {
+    jest.spyOn(newTransaction, "calculateHash").mockReturnValue(txHash)
+    newTransaction.signTransaction(privateKey)
+    expect(newTransaction.signature).toBe(txSignature)
+  });
 
 });
 
 describe('hasValidSignature', () => {
 
   beforeEach(() => {
-    newTransaction.timestamp = 1
-    newTransaction.signature = "30450221009053cc684f78c5173d389874fa556fde4f3314685650e58b89c1ac5ef920fc4c022042aae57e122bf5b13e75f45db4c3b1c59e7210b72197fa9b40ac3ca2955d395a"
-    newTransaction.fromAddress = publicKey
+    //Set to be valid signature
+    newTransaction.signature = txSignature
+  });
+
+  test('Returns true if signature matches fromAddress public key and all transaction details', () => {
+    expect(newTransaction.hasValidSignature()).toBe(true)
   });
 
   test('Returns false if missing signature', () => {
@@ -135,10 +151,6 @@ describe('hasValidSignature', () => {
     //Valid signature from another public/private key pair
     newTransaction.signature = "3045022100e89ec48b4645c96d64b138e2a1628ddcbe63e8c1eb47c29b3ba4c53a18aa004a02205288382a46f3c9d8ab9833a3f5300fcf38814873d0484b05f30e44b05fba20c0"
     expect(newTransaction.hasValidSignature()).toBe(false)
-  });
-
-  test('Returns true if signature matches fromAddress public key and all transaction details', () => {
-    expect(newTransaction.hasValidSignature()).toBe(true)
   });
 
   test('Returns false if amount changes', () => {
@@ -182,9 +194,7 @@ describe('hasValidSignature', () => {
     newTransaction.fromAddress = "0466106d7a83b41134c3e973fe13c0aa682ec8ee94c32cf6d66429e429554dc4ed9d7c64d0c5685229f503307a30f65c954e781d0f3cf4f77d07e0a9d4c37e1d01"
     expect(newTransaction.hasValidSignature()).toBe(false)
   });
-
 });
-
 
 describe('hasRequiredFields', () => {
 
@@ -214,17 +224,17 @@ describe('hasRequiredFields', () => {
 describe('isValid', () => {
 
   beforeEach(() => {
-    jest.spyOn(newTransaction, 'hasRequiredFields').mockImplementation(() => true);
-    jest.spyOn(newTransaction, 'hasValidSignature').mockImplementation(() => true);
+    jest.spyOn(newTransaction, 'hasRequiredFields').mockReturnValue(true);
+    jest.spyOn(newTransaction, 'hasValidSignature').mockReturnValue(true);
   })
 
   test('Returns false if hasRequiredFields returns false', () => {
-    jest.spyOn(newTransaction, 'hasRequiredFields').mockImplementation(() => false);
+    jest.spyOn(newTransaction, 'hasRequiredFields').mockReturnValue(false);
     expect(newTransaction.isValid()).toBe(false)
   });
 
   test('Returns false if hasValidSignature returns false', () => {
-    jest.spyOn(newTransaction, 'hasValidSignature').mockImplementation(() => false);
+    jest.spyOn(newTransaction, 'hasValidSignature').mockReturnValue(false);
     expect(newTransaction.isValid()).toBe(false)
   });
 
@@ -251,6 +261,7 @@ describe('CoinbaseTransaction subclass', () => {
   describe('Constructor', () => {
     it('Creates a transaction instance', () => {
       expect(coinbaseTx).toBeInstanceOf(Transaction)
+      expect(coinbaseTx).toBeInstanceOf(CoinbaseTransaction)
     });
   
     it('Has Coinbase Tx fields from config value', () => {
@@ -268,10 +279,8 @@ describe('CoinbaseTransaction subclass', () => {
     }); 
   
     it('Has no fee', () => {
-      expect(coinbaseTx).toMatchObject({
-        fee: 0
-      });
-    }); 
+      expect(coinbaseTx.fee).toBe(0)
+    });
   })
 
   describe('isValid', () => {
@@ -296,15 +305,12 @@ describe('CoinbaseTransaction subclass', () => {
       }); 
     })
 
-
     describe('Returns true', () => {
       it('Otherwise', () => {
         expect(coinbaseTx.isValid()).toBe(true)
       });
     })
- 
 
   })
-
 });
 
