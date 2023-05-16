bd2d70006003ca1c3260ae897c52ec20432b3343
Clay <clayblackiston@gmail.com>
2023-05-15 21:15:26 -0700

    Show full value on tooltip with hover for hash, prev hash, sender address, and receiver address

--- app/src/components/Block.tsx
+++ app/src/components/Block.tsx
@@ -1,4 +1,4 @@
-import { TableCell, TableRow } from "@mui/material";
+import { TableCell, TableRow, Tooltip } from "@mui/material";
 import { BlockType } from "../App";
 import { getTruncatedString } from "./MemPool/Transaction";
 
@@ -8,8 +8,20 @@ function Block({block}: {block: BlockType}) {
     <TableRow>
       <TableCell>{height}</TableCell>
       <TableCell>{new Date (timestamp).toLocaleString()}</TableCell>
-      <TableCell>{getTruncatedString(hash, 4)}</TableCell>
-      <TableCell sx={{ color: 'success.main' }}>{getTruncatedString(previousHash, 4)}</TableCell>
+      <TableCell>
+        <Tooltip title={hash} arrow>
+          <span>
+            {getTruncatedString(hash, 4)}
+          </span>
+        </Tooltip>
+      </TableCell>
+      <TableCell sx={{ color: 'success.main' }}>
+        <Tooltip title={previousHash} arrow>
+          <span>
+            {getTruncatedString(previousHash, 4)}
+          </span>
+        </Tooltip>
+      </TableCell>
       <TableCell>{transactions.length}</TableCell>
       <TableCell>{difficulty}</TableCell>
       <TableCell>{nonce}</TableCell>
--- app/src/components/MemPool/Transaction.tsx
+++ app/src/components/MemPool/Transaction.tsx
@@ -1,5 +1,5 @@
 
-import { TableRow, TableCell } from '@mui/material';
+import { TableRow, TableCell, Tooltip } from '@mui/material';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export const getTruncatedString = (string: String | null | undefined, limit: number) => {
@@ -18,9 +18,21 @@ export default function Transaction({transaction}) {
 
   return (
     <TableRow >
-      <TableCell>{getTruncatedString(transaction.fromAddress, 4)}</TableCell>
+      <TableCell>      
+        <Tooltip title={transaction.fromAddress} arrow>
+          <span>
+            {getTruncatedString(transaction.fromAddress, 4)}
+          </span>
+        </Tooltip>
+      </TableCell>
       <TableCell><ArrowForwardIcon/></TableCell>
-      <TableCell>{getTruncatedString(transaction.toAddress, 4)}</TableCell>
+      <TableCell>
+        <Tooltip title={transaction.toAddress} arrow>
+          <span>
+            {getTruncatedString(transaction.toAddress, 4)}
+          </span>
+        </Tooltip>
+      </TableCell>
       <TableCell sx={{ color: 'success.main' }}>{transaction.amount} ₿</TableCell>
       {/* @ts-ignore */}
     </TableRow>
