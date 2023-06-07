f3f2086cbe324c4a835b7a24c9a75c73efa489d1
Clay <clayblackiston@gmail.com>
2023-06-06 20:10:51 -0700

    Refactor syncChains into new file

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,10 @@
-import Blockchain from '../blockchain/Blockchain/Blockchain'
 import 'reflect-metadata';
 import 'es6-shim';
-import request from 'request'
-import { plainToClass } from 'class-transformer';
-import {blockchain } from './api/blockchain'
-import { PORT, DEFAULT_PORT, ROOT_NODE_ADDRESS } from './api/utils/ports';
-const server = require('./api/server');
 
-const syncChains = () => {
-  request({ url: `${ROOT_NODE_ADDRESS}/blockchain`}, (error: any, response: { statusCode: number }, body: string) => {
-    if (!error && response.statusCode === 200) {
-      const rootChain = JSON.parse(body)
-      let blockchainInstance = plainToClass(Blockchain, rootChain.blockchain);
-      console.log('replace chain on a sync with', blockchainInstance)
-      blockchain.replaceChain(blockchainInstance)
-    }
-  })
-}
+import { PORT, DEFAULT_PORT } from './api/utils/ports';
+import { syncChains } from './api/utils/syncChains';
+
+const server = require('./api/server');
 
 server.listen(PORT, () => {
   console.log(`Blockchain node running on port ${PORT}`)
