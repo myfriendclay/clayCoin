8840218d80d79ec52fed6fd7d38a585021174529
Clay <clayblackiston@gmail.com>
2023-06-12 15:20:27 -0700

    Fix console warning about p being direct descendant of table body

--- frontend/src/components/Transactions/Transactions.tsx
+++ frontend/src/components/Transactions/Transactions.tsx
@@ -1,40 +1,61 @@
-import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper, Typography } from "@mui/material"
-import { TransactionType } from "../../App"
-import Transaction from "./Transaction"
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  Typography,
+} from "@mui/material";
+import { TransactionType } from "../../App";
+import Transaction from "./Transaction";
 
-export default function Transactions({transactions}: {transactions: TransactionType[]}) {
-
-  const headers = ["Time", "Sender", "", "Receiver", "Amount", "Fee", "Memo" ]
+export default function Transactions({
+  transactions,
+}: {
+  transactions: TransactionType[];
+}) {
+  const headers = ["Time", "Sender", "", "Receiver", "Amount", "Fee", "Memo"];
 
   return (
-    <TableContainer component={Paper} sx={{ minWidth: "1100px", paddingTop: "15px"  }} elevation={2}>
-      <Typography variant="h5" gutterBottom component="div" sx={{ textAlign: "center", }}>
+    <TableContainer
+      component={Paper}
+      sx={{ minWidth: "1100px", paddingTop: "15px" }}
+      elevation={2}
+    >
+      <Typography
+        variant="h5"
+        gutterBottom
+        component="div"
+        sx={{ textAlign: "center" }}
+      >
         Transactions
       </Typography>
       <Table aria-label="transactions table">
         <TableHead>
           <TableRow>
-            {headers.map(header => 
-              <TableCell 
-                key={header} 
-                sx={{ fontWeight: 'bold', fontSize: 18 }}>
-                  {header}
-              </TableCell>)}
+            {headers.map((header) => (
+              <TableCell key={header} sx={{ fontWeight: "bold", fontSize: 18 }}>
+                {header}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          { transactions.length ?
-            transactions.map((transaction: TransactionType) => 
-              <Transaction 
-                transaction={transaction}
-                key={transaction.uuid}/>)
-            :
-            <Typography variant="body1" component="p" padding={"20px"}>
-              No transactions found. Sad!
-            </Typography>
-          }
+          {transactions.length ? (
+            transactions.map((transaction: TransactionType) => (
+              <Transaction transaction={transaction} key={transaction.uuid} />
+            ))
+          ) : (
+            <TableRow>
+              <TableCell>
+                <Typography>No transactions found. Sad!</Typography>
+              </TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
     </TableContainer>
-  )
+  );
 }
